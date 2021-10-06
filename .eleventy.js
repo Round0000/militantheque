18560cf61c0a328559c4a37e632f46239e84b7d0
Round0000 <billybinche@gmail.com>
2021-10-06 16:50:45 +0200

    commit

--- .eleventy.js
+++ .eleventy.js
@@ -1,16 +1,21 @@
 module.exports = function (eleventyConfig) {
-    eleventyConfig.addPassthroughCopy("_includes/assets/css/styles.css");
-    eleventyConfig.addWatchTarget("_includes/assets/css/styles.css");
-    eleventyConfig.addPassthroughCopy("./src/js");
-    eleventyConfig.addPassthroughCopy("./src/admin");
-    eleventyConfig.addPassthroughCopy("./src/images");
-    eleventyConfig.addPassthroughCopy({ "./src/favicon": "/" });
-  
-    return {
-      dir: {
-        input: "src",
-        output: "public",
-      },
-      passthroughFileCopy: true
-    };
+  eleventyConfig.addPassthroughCopy("_includes/assets/css/styles.css");
+  eleventyConfig.addWatchTarget("_includes/assets/css/styles.css");
+  eleventyConfig.addPassthroughCopy("./src/_includes/assets/js");
+  eleventyConfig.addPassthroughCopy("./src/admin");
+  eleventyConfig.addPassthroughCopy("./src/images");
+  eleventyConfig.addPassthroughCopy({ "./src/favicon": "/" });
+
+  eleventyConfig.addFilter("onlyText", (body) => {
+    const content = body.replace(/(<([^>]+)>)/gi, "");
+    return content.replace(/(\r\n|\n|\r)/gm, "");
+  });
+
+  return {
+    dir: {
+      input: "src",
+      output: "public",
+    },
+    passthroughFileCopy: true,
   };
+};
---
+++ src/_includes/assets/js/search.js
@@ -0,0 +1,50 @@
+document.addEventListener("DOMContentLoaded", function (event) {
+  const search = document.getElementById("search");
+  const results = document.getElementById("results");
+  let data = [];
+  let search_term = "";
+
+  fetch("/search.json")
+    .then((response) => response.json())
+    .then((data_server) => {
+      data = data_server;
+    });
+
+  search.addEventListener("input", (event) => {
+    search_term = event.target.value.toLowerCase().trim();
+    showList();
+  });
+
+  const showList = () => {
+    results.innerHTML = "";
+    if (document.querySelector(".noresult") || search_term.length <= 0) {
+      document.querySelector(".noresult").remove();
+    }
+    if (search_term.length <= 0) return;
+    const match = new RegExp(`${search_term}`, "gi");
+    let result = data.filter(
+      (query) =>
+        match.test(query.title) ||
+        match.test(query.source) ||
+        match.test(query.extrait) ||
+        match.test(query.thematiques)
+    );
+    if (result.length === 0) {
+      const li = document.createElement("li");
+      li.innerHTML = `Aucun résultat trouvé pour <span class="query">"${search_term}"</span>`;
+      li.classList.add("noresult");
+      results.appendChild(li);
+    }
+    result.forEach((e) => {
+      const li = document.createElement("li");
+      li.innerHTML = `
+        <a href="${e.lien}" class="item">
+            <h2>${e.title}</h2>
+            <span>${e.source}</span>
+            <p>${e.extrait}</p>
+        </a>
+      `;
+      results.appendChild(li);
+    });
+  };
+});
